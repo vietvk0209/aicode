0b8a5f10a62b554ee20b61326fd9b256c82cb4e6
vietvk0209 <vietchip5bk44@gmail.com>
2026-09-13 19:20:26 +0700

    Just test

--- src/main/java/com/company/CompanyApplication.java
+++ src/main/java/com/company/CompanyApplication.java
@@ -7,5 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CompanyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyApplication.class, args);
-	}
+	} 
 }
